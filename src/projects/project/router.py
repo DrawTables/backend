bda62e14296cf28c3d13e8203fd45fe2868edd1a
Anton Shushmentsev <shushmencev97@mail.ru>
2025-05-16 02:16:20 +0700

    Исправление опечаток в путях роутов

--- src/projects/project/router.py
+++ src/projects/project/router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends, Response, status
 
-
-from src.projects.project import use_cases
-from src.projects.project.dependencies import project_by_id_exists
 from src.auth.tokens.dependencies import (
     get_current_user as current_user,
 )
+from src.projects.project import use_cases
+from src.projects.project.dependencies import project_by_id_exists
 from src.projects.project.schemas import (
     ProjectCreateRequest,
     ProjectResponse,
@@ -21,7 +20,7 @@ projects_router_v1 = APIRouter(
 
 
 @projects_router_v1.get(
-    path="{project_id}",
+    path="/{project_id}",
     response_model=ProjectResponse,
     dependencies=[Depends(project_by_id_exists)],
 )
@@ -39,13 +38,13 @@ async def create_project(
     project_create_request: ProjectCreateRequest,
     response: Response,
     user: dict = Depends(current_user),
-) :
+):
     project_id = await use_cases.create_project(project_create_request, user.user_id)
     response.headers["Location"] = f"{ROUTER_V1_PREFIX}/{project_id}"
-    
-    
+
+
 @projects_router_v1.patch(
-    path="{project_id}",
+    path="/{project_id}",
     status_code=status.HTTP_204_NO_CONTENT,
     dependencies=[Depends(project_by_id_exists)],
 )
@@ -53,13 +52,11 @@ async def update_project(
     project_id: str,
     project_update_request: ProjectUpdateRequest,
 ):
-    await use_cases.update_project(
-        project_id, project_update_request
-    )
+    await use_cases.update_project(project_id, project_update_request)
 
 
 @projects_router_v1.delete(
-    path="{project_id}",
+    path="/{project_id}",
     status_code=status.HTTP_204_NO_CONTENT,
     dependencies=[Depends(project_by_id_exists)],
 )
